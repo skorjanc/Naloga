8b97b7b76e3b6b833ccd71a2ba04d0384d7ab0b4
skorjanc <44368207+skorjanc@users.noreply.github.com>
2018-10-22 21:37:57 +0200

    Update DN4.cpp

--- DN4.cpp
+++ DN4.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <cmath>
 #include <ctime>
@@ -7,7 +6,7 @@ using namespace std;
 int main()
 {
 	int ura, min;
-    cout << "Vnesi cas: Ure: "; 
+    	cout << "Vnesi cas: Ure: "; 
 	cin >> ura;
 	cout << ", minute: ";
 	cin >> min;
@@ -15,10 +14,10 @@ int main()
 	double kotu, kotm, kot;
 	
 	kotm = min * 6;
-	kotu = ura * 30;
-	kot = fabs(kotu - kotm);
+	kotu = ura * 30; //Če želimo da se za vsako minuto premakne tudi urni kazalec: kotu = ura * 30 + kotm * 0.5; ali kotu = fma(ura, 30, kotm/2);
+	kot = fabs(kotu - kotm); 
 	if (kot > 180) {
-		kot = fabs(360 - kot);
+		kot = fabs(360 - kot); //ne vem zakaj ampak brez absolutne vrednosti je včasih kot negativen primer: (18:19)
 	}
 
 	cout << "Urni in minutni kazalec ob " << ura << ":" << min << " oklepata kot " << kot << "°." << endl;
